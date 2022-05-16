428def1e2370ba5b9398079cc70f842610465e89
@A-McMullin <a.p.mcmullin@gmail.com>
2022-05-15 20:03:53 -0500

    1st commit

--- src/GITHW1/GITHomework1.java
+++ src/GITHW1/GITHomework1.java
@@ -3,8 +3,8 @@ package GITHW1;
 public class GITHomework1 {
     public static void main(String[] args) {
 
-        for (int i = 0; i<=100; i++){
-            System.out.println(i);
+        for (int a = 0; a<=100; a++){
+            System.out.println(a);
         }
     }
 }
